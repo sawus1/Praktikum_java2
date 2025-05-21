2cd089374ff1a2887563265e05fab8eeb3f357af
0nikun <ino.woelke@gmail.com>
2025-05-21 12:26:17 +0200

    Put Methode, Stop Methode, Dateispeichern wenn datei nicht leer

--- Aufgabe_5/src/KlausurenServer.java
+++ Aufgabe_5/src/KlausurenServer.java
@@ -1,64 +1,77 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class KlausurenServer{
-	
+public class KlausurenServer {
+
 	private static Map<String, ArrayList<Integer>> KlausurInfos = new HashMap<>();
-	
+	private static boolean run = true;
+
 	public static void main(String[] args) {
-		ServerSocket serverSo = null;
+
 		int port = Integer.valueOf(args[0]).intValue();
-		try {
-			serverSo = new ServerSocket(port);
+		try (ServerSocket server = new ServerSocket(port)) {
+			File KlausurInfoDatei = new File("/home/ino/Praktikum_java2/Aufgabe_5/src/KlausurenInformation");
+
+			try (ObjectInputStream initialdatei = new ObjectInputStream(new FileInputStream(KlausurInfoDatei));) {
+				KlausurInfos = (Map<String, ArrayList<Integer>>) initialdatei.readObject();
+			}
+
+			while (run) {
+				Socket client = server.accept();
+				KlausurenServerThread t = new KlausurenServerThread(client);
+				t.start();
+				t.join();
+				client.close();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		while(true) {
-	
-			try {
-				Socket client = serverSo.accept();
-				new KlausurenServerThread(client);
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-		}
-		
+
+		System.exit(0);
+
 	}
-	
-	
-	public static Map<String,ArrayList<Integer>> getKlausurenInfor() {
+
+	public static Map<String, ArrayList<Integer>> getKlausurenInfor() {
 		return KlausurInfos;
 	}
-	
+
 	public static ArrayList<Integer> putValue(String key, ArrayList<Integer> value) {
-		
+
 		return KlausurInfos.put(key, value);
-		
+
 	}
 
-	public String getValue(String key) {
+	public static String getValue(String key) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public boolean deleteValue(String key) {
+	public static boolean deleteValue(String key) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public String getAllKlausuren() {
+	public static String getAllKlausuren() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public boolean stopServer() {
-		// TODO Auto-generated method stub
-		return false;
+	public static boolean stopServer() {
+		run = false;
+
+		return true;
 	}
 
 }
--- Aufgabe_5/src/KlausurenServerThread.java
+++ Aufgabe_5/src/KlausurenServerThread.java
@@ -11,58 +11,75 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class KlausurenServerThread extends Thread{
+public class KlausurenServerThread extends Thread {
 	Socket ClientSocket;
-	
+
 	public KlausurenServerThread(Socket client) {
 		ClientSocket = client;
 	}
 
+	public String getKey(String line) {
+		return line.split(" ")[1];
+	}
+
+	public ArrayList<Integer> getValue(String line) {
+		ArrayList<Integer> value = new ArrayList<>();
+		String[] stringValues = line.split(" ")[2].replaceAll("\\[|\\]| ", "").split(",");
+		for (String s : stringValues)
+			value.add(Integer.parseInt(s));
+
+		return value;
+	}
+
 	@Override
 	public void run() {
-		try {
-			BufferedReader eingang = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
-			PrintWriter serverAntwort = new PrintWriter(ClientSocket.getOutputStream(),true);
-			ObjectOutputStream dateiSpeichern = new ObjectOutputStream(new FileOutputStream("/Aufgabe_5/src/KlausurenInformation"));
-				
-			String anfrage;
-			String methode = "";
-			String key = "";
-			ArrayList<Integer> value = new ArrayList<>();
-			
+		try (BufferedReader eingang = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
+				PrintWriter serverAntwort = new PrintWriter(ClientSocket.getOutputStream(), true);
+				ObjectOutputStream dateiSpeichern = new ObjectOutputStream(
+						new FileOutputStream("/home/ino/Praktikum_java2/Aufgabe_5/src/KlausurenInformation"));) {
+			String anfrage = eingang.readLine();
 			StringBuilder antwort = new StringBuilder();
-			while((anfrage = eingang.readLine()) != null) {
-				methode = anfrage.split(" ")[0];
-				key = anfrage.split(" ")[1];
-				for(String s : anfrage.split(" ")[2].replaceAll("[", "").replaceAll("]", "").split(","))
-					value.add(Integer.parseInt(s));
-				
-			}
-			switch (methode){
-				case "PUT": 
-					System.out.println("PUT");
-					antwort.append(KlausurenServer.putValue(key, value));
-				case "GET":
-					
-				case "DEL":
-					
-				case "GETALL":
-					
-				case "STOP":
-					
+
+			System.out.println(anfrage);
+			String methode = anfrage.split(" ")[0];
+			System.out.println("Methode: " + methode);
+			
+			switch (methode) {
+			case "PUT":
+				System.out.println("PUT");
+				antwort.append("1 ");
+				antwort.append(KlausurenServer.putValue(getKey(anfrage), getValue(anfrage)));
+				break;
+			case "GET":
+				System.out.println(methode);
+				break;
+			case "DEL":
+				System.out.println(methode);
+				break;
+			case "GETALL":
+				System.out.println(methode);
+				antwort.append("1 ");
+				antwort.append(KlausurenServer.getAllKlausuren());
+				break;
+			case "STOP":
+				System.out.println(methode);
+
+				if (KlausurenServer.stopServer())
+					antwort.append("1");
+				else
+					antwort.append("0");
+				break;
+			default:
+				antwort.append("0 falsche Anfrage");
 			}
 			dateiSpeichern.writeObject(KlausurenServer.getKlausurenInfor());
 			serverAntwort.println(antwort.toString());
-			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
-	
-	
 }
